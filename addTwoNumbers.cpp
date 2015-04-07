6b9d46c234b2dbceffbca4aba0bc847f1a633f1d
MarinYoung4596 <marinyoung@163.com>
2015-04-07 18:42:18 +0800

    add two numbers

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "ListNode.h"
+
+using namespace std;
+
+int add(int a, int b, int &carry)
+{
+    int c = a + b + carry;
+    carry = c / 10;
+    return c % 10;
+}
+
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+{
+    if (l1 == NULL)
+        return l2;
+    if (l2 == NULL)
+        return l1;
+
+    ListNode *head = NULL, *rear = NULL, *new_node = NULL;
+    int carry = 0, val = 0;
+    while (l1 || l2 )
+    {
+        if (!l1)
+        {
+            val = add(0, l2->val, carry);
+            l2 = l2->next;
+        }
+        else if (!l2)
+        {
+            val = add(l1->val, 0, carry);
+            l1 = l1->next;
+        }
+        else
+        {
+            val = add(l1->val, l2->val, carry);
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+        new_node = new ListNode(val);
+        if (!head)
+            head = new_node;
+        if (!rear)
+            rear = new_node;
+        else
+        {
+            rear->next = new_node;
+            rear = rear->next;
+        }
+    }
+
+    if (carry != 0 && rear != NULL)
+    {
+        rear->next = new ListNode(carry);
+    }
+    return head;
+}
