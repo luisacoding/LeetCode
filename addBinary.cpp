c414bb504d2cb0110eac31f1e7fed8074df9f04d
MarinYoung4596 <marinyoung@163.com>
2015-04-21 21:42:49 +0800

    add binary

--- addBinary.cpp
+++ addBinary.cpp
@@ -4,49 +4,68 @@
 //a = "11"
 //b = "1"
 //Return "100".
-#include <cstring>
+#include <string>
+#include <sstream>
+#include <iostream>
 
-string binaryAdder(char a, char b, int &carry)
+string binaryAdder(char aa, char bb, int &c)
 {
-    int aa = (int)(a - '0'), bb = (int)(b - '0'), res;
-    cout<<carry<<'\t'<<aa<<'\t'<<bb<<endl;
+	int a = (int)(aa - '0'),
+		b = (int)(bb - '0'),
+		res;
 
-    carry = (aa & bb) | (carry & (aa ^ bb));
-    res = (aa ^ bb) ^ carry;
-
-    cout<<"carry: "<<carry<<"\tresult: "<<res<<endl;
-    char result[1];
-    sprintf(result, "%d", res);
-    return result;
+	res = (~aa & ~b & c) | (~a & b & ~c) | (a & ~b & ~c) | (a & b & c);
+	c = (a & ~b & c) | (a & b & ~c) | (b & c);
+	
+	stringstream ss;
+	string s;
+	ss << res;
+	ss >> s;
+	return s;
 }
 
 string addBinary(string a, string b)
 {
-    if (a == "") return b;
-    if (b == "") return a;
+	if (a == "") return b;
+	if (b == "") return a;
 
-    string res = "";
-    int indexA = a.length() - 1;
-    int indexB = b.length() - 1;
-    int carry = 0;
-    string bit;
-    for (; indexA >= 0 && indexB >= 0; indexA--, indexB--)
-    {
-        bit = binaryAdder(a[indexA], b[indexB], carry);
-        res = bit + res;
-    }
-    while (indexA >= 0)
-    {
-        bit = binaryAdder(a[indexA--], '0', carry);
-        res = bit + res;
-    }
-    while (indexB >= 0)
-    {
-        bit = binaryAdder('0', b[indexB--], carry);
-        res = bit + res;
-    }
-    if (carry)
-        res.insert(res.begin(), '1');
-    return res;
+	string res = "";
+	int indexA = a.length() - 1;
+	int indexB = b.length() - 1;
+	int carry = 0;
+	string bit;
+	for (; indexA >= 0 && indexB >= 0; indexA--, indexB--)
+	{
+		bit = binaryAdder(a[indexA], b[indexB], carry);
+		res = bit + res;
+	}
+	while (indexA >= 0)
+	{
+		bit = binaryAdder(a[indexA--], '0', carry);
+		res = bit + res;
+	}
+	while (indexB >= 0)
+	{
+		bit = binaryAdder('0', b[indexB--], carry);
+		res = bit + res;
+	}
+	if (carry)
+		res.insert(res.begin(), '1');
+	return res;
+}
+
+int main(int argc, char **argv)
+{
+	string a = "001",
+		b = "011";
+	
+	if (argc == 3)
+	{
+		a = argv[1];
+        a = argv[2];
+	}
+	std::cout << addBinary(a, b) << std::endl;
+	
+	return 0;
 }
 
