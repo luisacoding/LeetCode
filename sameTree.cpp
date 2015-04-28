f146f631603d3392fc6b1ff9ccca3d12edbfffe9
MarinYoung4596 <marinyoung@163.com>
2015-04-28 21:24:40 +0800

    DFS

---
+++ sameTree.cpp
@@ -0,0 +1,21 @@
+#include "TreeNode.h"
+
+/*
+100. Same Tree
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+*/
+
+// DFS
+bool isSameTree(TreeNode *p, TreeNode *q)
+{
+	if (p == nullptr && q == nullptr)
+		return true;
+	else if ((p == nullptr &&  q != nullptr) || (p != nullptr &&  q == nullptr) )
+		return false;
+	else if (p->val != q->val)
+		return false;
+
+	return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
